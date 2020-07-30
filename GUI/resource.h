1436fed745a47113d5537d48d27056cbf259946e
Michael B <downwithup@protonmail.com>
2020-07-30 17:28:24 -0400

    Create resource.h

---
+++ GUI/resource.h
@@ -0,0 +1,26 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDD_DIALOG1                     101
+#define IDC_BTN_INIT                    1003
+#define IDC_EDIT_ADD                    1004
+#define IDC_LIST_MAIN                   1006
+#define IDC_BTN_ADD                     1008
+#define IDC_BTN_REMOVE                  1009
+#define IDC_STATIC_BOX                  1010
+#define IDC_MEMO_STACK                  1011
+#define IDC_STATIC_LABEL                1012
+#define IDC_BUTTON1                     1013
+#define IDC_BUTTON_CLEAR                1013
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
